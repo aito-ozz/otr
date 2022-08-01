62e7e17d1a168e34ce3c491558bfc9307bc9fe86
aito-ozz <91691171+aito-ozz@users.noreply.github.com>
2022-08-01 16:33:54 +0500

    Update test_otr.py

--- test_otr.py
+++ test_otr.py
@@ -6,7 +6,7 @@ from variables import *
 def test_org_teh_resh(browser):
     page = MainPage(browser, url)
     page.open()
-    # page.header()
-    # page.all_links()
-    # page.link_request()
-    # page.adress_bar()
+    page.header()
+    page.all_links()
+    page.link_request()
+    page.adress_bar()
